ee688707bf6ec0bb9d4668fc81241df9c810a73f
rabatscher <p.rabatscher@hotmail.com>
2023-09-27 12:55:07 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        honey: ['"hellohoney"'],
+        book: ['"Book Antiqua"'],
+      },
+      animation: {
+        hover: "hover 3s ease-in-out infinite",
+      },
+      keyframes: {
+        hover: {
+          "0%, 100%": { transform: "translateY(0vh)" },
+          "50%": { transform: "translateY(2vh)" },
+        },
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
